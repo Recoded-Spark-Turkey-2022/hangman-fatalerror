c67431e36111af7580d9c6e0c2ca3ffb5196e901
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-11-09 10:17:35 +0300

    Letter selection function added.

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,8 @@ for(let i = 0; i < randomWord[0].length; i++){
     letters.push(randomWord[0].charAt(i)) //can be deleted later, created for test purposes
 
     const li = document.createElement('li')
-    li.textContent = randomWord[0].charAt(i).toUpperCase()
+    li.textContent = '_'
+    li.classList = letters[i]
     ul.appendChild(li)
     
 }
@@ -23,3 +24,65 @@ console.log(letters)
 
 
 })
+
+
+const ulOfAlphabet = document.createElement('ul')
+    ulOfAlphabet.type = 'none'
+const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+alphabet.forEach(element => {
+    const liOfAlphabet = document.createElement('li')
+    const btnOfAlphabet = document.createElement('button')
+        btnOfAlphabet.textContent = element
+        btnOfAlphabet.style.width = '3em'
+
+        btnOfAlphabet.addEventListener('click', () => {
+            for(let i = 0; i < letters.length; i++){
+                if(letters[i] == btnOfAlphabet.textContent){
+                    btnOfAlphabet.disabled = 'true'
+                    btnOfAlphabet.style.background = 'green'
+
+                document.querySelectorAll(`.${letters[i]}`).forEach(letter => {
+                    letter.textContent = letters[i]
+                })
+
+                }else{
+                    btnOfAlphabet.disabled = 'true'
+
+
+            }
+            }
+        })
+
+    liOfAlphabet.appendChild(btnOfAlphabet)
+    ulOfAlphabet.appendChild(liOfAlphabet)    
+})
+document.querySelector('body').appendChild(ulOfAlphabet)
+
